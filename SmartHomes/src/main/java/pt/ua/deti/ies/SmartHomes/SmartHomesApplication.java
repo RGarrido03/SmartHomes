4b7c96c54bf4dad29976c8c6d3b732c2e3ce87cf
SlicF <fabiomatias39@ua.pt>
2023-11-16 11:57:00 +0000

    Project Backbone

---
+++ SmartHomes/src/main/java/pt/ua/deti/ies/SmartHomes/SmartHomesApplication.java
@@ -0,0 +1,13 @@
+package pt.ua.deti.ies.SmartHomes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHomesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHomesApplication.class, args);
+	}
+
+}
---
+++ SmartHomes/src/test/java/pt/ua/deti/ies/SmartHomes/SmartHomesApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.ua.deti.ies.SmartHomes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHomesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
