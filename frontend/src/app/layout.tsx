50890deeee99ffb66ab8d4c1d99a6ef54781e2e6
falcaodiogo <falcao.diogo@ua.pt>
2023-11-23 12:24:58 +0000

    material symbols

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import "react-material-symbols/dist/rounded.css"
 import { cn } from '@/lib/utils'
 import { ThemeProvider } from '@/components/theme-provider'
 
