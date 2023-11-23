3d9b4d99cbf6f99d8881fe81ea2f69ece1645d08
falcaodiogo <falcao.diogo@ua.pt>
2023-11-23 12:21:48 +0000

    add dark mode support

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { cn } from '@/lib/utils'
+import { ThemeProvider } from '@/components/theme-provider'
 
 const inter = Inter({ subsets: ['latin'], variable: "--font-sans", })
 
@@ -16,8 +17,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={cn("min-h-screen bg-background font-sans antialiased", inter.variable)}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={cn("min-h-screen bg-background font-sans antialiased", inter.variable)}> <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+          > {children}</ThemeProvider></body>
     </html>
   )
 }
