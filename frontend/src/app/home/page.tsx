70e91d2e4b3e657f5112354a57e41c0f800ebf53
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-11-24 23:48:18 +0000

    Navbar

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -1,10 +1,11 @@
+import Navbar from "@/components/navbar";
 import Sidebar from "@/components/sidebar";
 
 export default function Home() {
   return (
-    <div>
-      <p>Hi how</p>
-      <Sidebar />
-    </div>
+    <>
+      <Navbar />
+      <Sidebar active="home" />
+    </>
   );
 }
