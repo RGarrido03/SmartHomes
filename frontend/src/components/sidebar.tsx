d70a0798fdd2e12534874c0772977b874be46ce0
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-11-25 19:54:51 +0000

    Export tabs to a new file

--- frontend/src/components/sidebar.tsx
+++ frontend/src/components/sidebar.tsx
@@ -1,71 +1,13 @@
-import { MaterialSymbol, MaterialSymbolProps } from "react-material-symbols";
+import { MaterialSymbol } from "react-material-symbols";
 import { Button } from "./ui/button";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
-
-type SidebarTabsProps = {
-  id:
-    | "home"
-    | "environment"
-    | "electricity"
-    | "water"
-    | "devices"
-    | "costs"
-    | "settings";
-  name: string;
-  icon: MaterialSymbolProps["icon"];
-  href: string;
-}[];
+import { tabs } from "@/app/home/tabs";
 
 type SidebarProps = {
   activeUrl: string;
   className?: string;
 };
-
-const tabs: SidebarTabsProps = [
-  {
-    id: "home",
-    name: "Home",
-    icon: "home",
-    href: "/home",
-  },
-  {
-    id: "environment",
-    name: "Environment",
-    icon: "nest_eco_leaf",
-    href: "/home/environment",
-  },
-  {
-    id: "electricity",
-    name: "Electricity",
-    icon: "bolt",
-    href: "/home/electricity",
-  },
-  {
-    id: "water",
-    name: "Water",
-    icon: "water_drop",
-    href: "/home/water",
-  },
-  {
-    id: "devices",
-    name: "Devices",
-    icon: "scene",
-    href: "/home/devices",
-  },
-  {
-    id: "costs",
-    name: "Costs",
-    icon: "euro_symbol",
-    href: "/home/costs",
-  },
-  {
-    id: "settings",
-    name: "Settings",
-    icon: "settings",
-    href: "/home/settings",
-  },
-];
 
 export default function Sidebar({ activeUrl, className }: SidebarProps) {
   return (
