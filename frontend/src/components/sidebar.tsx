bcfd24aa4058759ece76fc933ce7817d8be9b11b
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-11-26 02:35:57 +0000

    Add tooltip on tabs (desktop version)

--- frontend/src/components/sidebar.tsx
+++ frontend/src/components/sidebar.tsx
@@ -3,6 +3,12 @@ import { Button } from "./ui/button";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { tabs } from "@/app/home/tabs";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 type SidebarProps = {
   activeUrl: string;
@@ -18,35 +24,44 @@ export default function Sidebar({ activeUrl, className }: SidebarProps) {
         className,
       )}
     >
-      {tabs.map((tab) => (
-        <Link href={tab.href} key={tab.id} className="snap-start">
-          {/* Desktop version button */}
-          <Button
-            variant={activeUrl === tab.href ? "default" : "ghost"}
-            className="hidden rounded-lg p-2 md:block"
-          >
-            <MaterialSymbol
-              size={24}
-              icon={tab.icon}
-              fill={activeUrl === tab.href}
-            />
-            <p className="ml-2 md:hidden">{tab.name}</p>
-          </Button>
+      <TooltipProvider>
+        {tabs.map((tab) => (
+          <Link href={tab.href} key={tab.id} className="snap-start">
+            {/* Desktop version button */}
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  variant={activeUrl === tab.href ? "default" : "ghost"}
+                  className="hidden rounded-lg p-2 md:block"
+                >
+                  <MaterialSymbol
+                    size={24}
+                    icon={tab.icon}
+                    fill={activeUrl === tab.href}
+                  />
+                  <p className="ml-2 md:hidden">{tab.name}</p>
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>{tab.name}</p>
+              </TooltipContent>
+            </Tooltip>
 
-          {/* Mobile version pile-like button */}
-          <Button
-            variant={activeUrl === tab.href ? "default" : "background"}
-            className="rounded-full px-4 py-2 md:hidden"
-          >
-            <MaterialSymbol
-              size={24}
-              icon={tab.icon}
-              fill={activeUrl === tab.href}
-            />
-            <p className="ml-2 md:hidden">{tab.name}</p>
-          </Button>
-        </Link>
-      ))}
+            {/* Mobile version pile-like button */}
+            <Button
+              variant={activeUrl === tab.href ? "default" : "background"}
+              className="rounded-full px-4 py-2 md:hidden"
+            >
+              <MaterialSymbol
+                size={24}
+                icon={tab.icon}
+                fill={activeUrl === tab.href}
+              />
+              <p className="ml-2 md:hidden">{tab.name}</p>
+            </Button>
+          </Link>
+        ))}
+      </TooltipProvider>
     </div>
   );
 }
