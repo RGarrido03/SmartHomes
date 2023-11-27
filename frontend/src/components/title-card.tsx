938eefc771ab45e71d1f0e37f0e012e1de98e968
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-11-27 01:36:39 +0000

    Refactor title card to internally use shadcn one

--- frontend/src/components/title-card.tsx
+++ frontend/src/components/title-card.tsx
@@ -1,7 +1,11 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+
 export function TitleCard({ text }: { text: string }) {
   return (
-    <div className="rounded-card bg-card p-4 md:col-span-2 lg:col-span-3">
-      <p className="text-xl font-bold">{text}</p>
-    </div>
+    <Card className="md:col-span-2 lg:col-span-3">
+      <CardHeader>
+        <CardTitle>{text}</CardTitle>
+      </CardHeader>
+    </Card>
   );
 }
