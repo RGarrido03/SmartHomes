5140596d34d5fad4135f05424af423955294ec33
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-11-27 20:12:12 +0000

    Temporarily remove InfluxDB

--- backend/src/main/java/pt/ua/deti/ies/SmartHomes/backend/BackendApplication.java
+++ backend/src/main/java/pt/ua/deti/ies/SmartHomes/backend/BackendApplication.java
@@ -3,16 +3,12 @@ package pt.ua.deti.ies.SmartHomes.backend;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import pt.ua.deti.ies.SmartHomes.backend.InfluxDB.InfluxDBConfig;
 
 @SpringBootApplication
 @EnableRabbit
 @EnableScheduling
 
-//import configuration of InfluxDB
-@Import(InfluxDBConfig.class)
 public class BackendApplication {
 
 	public static void main(String[] args) {
