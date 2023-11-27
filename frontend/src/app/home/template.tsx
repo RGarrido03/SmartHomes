563a148410ff6b792ed1ab027c52feffa49ff3d4
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-11-27 01:00:15 +0000

    Delete unnecessary margin on mobile

--- frontend/src/app/home/template.tsx
+++ frontend/src/app/home/template.tsx
@@ -12,7 +12,7 @@ export default function Template({ children }: { children: React.ReactNode }) {
       <Navbar />
       <div className="flex flex-1 flex-col gap-4 px-4 md:flex-row md:px-0">
         <Sidebar activeUrl={pathname} className="md:self-stretch" />
-        <div className="mr-4 flex-1">{children}</div>
+        <div className="flex-1 md:mr-4">{children}</div>
       </div>
     </>
   );
