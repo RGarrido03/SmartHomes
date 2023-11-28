778668fca205e7c32806e42358145ae3ed066f0b
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-11-28 01:32:37 +0000

    Revert "Temporarily remove InfluxDB"

--- backend/src/main/java/pt/ua/deti/ies/SmartHomes/backend/InfluxDB/InfluxDBConfig.java
+++ backend/src/main/java/pt/ua/deti/ies/SmartHomes/backend/InfluxDB/InfluxDBConfig.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @Configuration
 public class InfluxDBConfig {
@@ -15,43 +17,41 @@ public class InfluxDBConfig {
     @Value("${spring.influx.url}")
     private String influxUrl;
 
-    @Value("${spring.influx.username}")
+    @Value("${spring.influx.user}")
     private String username;
 
     @Value("${spring.influx.password}")
     private String password;
 
-    @Value("${spring.influxdb.database}")
+    @Value("${spring.influx.database}")
     private String database;
 
-    @Value("${spring.influxdb.token}")
+    @Value("${spring.influx.token}")
     private String token;
 
-    @Value("${spring.influxdb.bucket}")
-    private String bucket;
+    @Value("${spring.influx.bucket}")
+    public String bucket;
 
-    @Value("${spring.influxdb.org}")
+    @Value("${spring.influx.org}")
     private String org;
-
 
     @Bean
     public InfluxDBClient influxDBClient() {
-        return InfluxDBClientFactory.create(influxUrl, token.toCharArray());
+        return InfluxDBClientFactory.create(influxUrl, token.toCharArray(), org, bucket);
     }
 
     @Autowired
+    @Lazy
     private InfluxDBClient influxDBClient;
 
-
-    //example of an insert
+    @Scheduled(fixedDelay = 3000L)
     public void writeData() {
-        WriteApi writeApi = influxDBClient.getWriteApi();
-
+        WriteApi writeApi = influxDBClient.makeWriteApi();
 
         Point point = Point.measurement("cpu")
-                .addField("idle", 90L)
-                .addField("user", 9L)
-                .addField("system", 1L);
+                           .addField("idle", 90L)
+                           .addField("user", 9L)
+                           .addField("system", 1L);
 
         writeApi.writePoint(bucket, org, point);
     }
