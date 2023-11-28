e619083c8ac6ea7a926e10fa71895d3c12e67994
Diogo Falcão <109617044+falcaodiogo@users.noreply.github.com>
2023-11-28 11:33:27 +0000

    Revert "9 create databases"

--- backend/src/main/java/pt/ua/deti/ies/SmartHomes/backend/Database/DeviceType.java
+++
@@ -1,5 +0,0 @@
-package pt.ua.deti.ies.SmartHomes.backend.Database;
-
-enum DeviceType {
-    SOLARPV, WINDTURBINE, PLUG, GRIDMETER, AC, LIGHT, TV, SMARTASSISTANT, CARCARGER, VACUUM, DESUMIDIFIER, OVEN
-}
