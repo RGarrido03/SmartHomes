75c2552592d13f2ff6e99e2e46d9d3809d41f5d0
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-11-30 01:08:57 +0000

    Revert "Os dados ainda não são encriptados"
    This reverts commit df2fe40caafafe4f3deaff255235f69e92865d22.

--- backend/src/main/java/pt/ua/deti/ies/SmartHomes/backend/LoginAndRegister/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package pt.ua.deti.ies.SmartHomes.backend.LoginAndRegister;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import pt.ua.deti.ies.SmartHomes.backend.Database.Client;
-
-public interface ClientRepository extends JpaRepository<Client, Long> {
-    Client findByUsername(String username);
-}
--- backend/src/main/java/pt/ua/deti/ies/SmartHomes/backend/LoginAndRegister/ClientService.java
+++
@@ -1,9 +0,0 @@
-package pt.ua.deti.ies.SmartHomes.backend.LoginAndRegister;
-
-
-import pt.ua.deti.ies.SmartHomes.backend.Database.Client;
-
-public interface ClientService {
-    Client save(Client client);
-    Client findByUsername(String username);
-}
