1fb03f985174e14d315d202b9ec266ed32708e1e
falcaodiogo <falcao.diogo@ua.pt>
2023-12-05 15:07:36 +0000

    minimize insight page ui

--- frontend/src/app/insight/page.tsx
+++ frontend/src/app/insight/page.tsx
@@ -7,34 +7,34 @@ export default function Home() {
     <div className="grid h-full flex-1 grid-cols-2">
       <div className="grid h-full content-center rounded-tr-2xl bg-background">
         <div className="row-auto grid content-center space-y-12 p-24">
-          <p className="text-6xl font-extrabold">
+          <p className="text-5xl font-extrabold">
             Welcome back, <br></br>John!
           </p>
-          <p className="text-secondary-foreground text-4xl font-medium">
+          <p className="text-3xl font-medium text-secondary-foreground">
             Here’s your summary.
           </p>
 
           <div className="relative rounded-xl p-4">
             <div className="absolute inset-0 animate-pulse rounded-xl border-4 border-secondary delay-100"></div>
             <div className="flex">
-              <div className="flex-1 relative z-10 flex gap-x-2">
-                <MaterialSymbol icon="bolt" size={32}></MaterialSymbol>
-                <p className="text-2xl font-medium">Electricity</p>
+              <div className="relative z-10 flex flex-1 gap-x-2">
+                <MaterialSymbol icon="bolt" size={24}></MaterialSymbol>
+                <p className="text-xl font-medium">Electricity</p>
               </div>
-              <p className="flex-1 text-2xl font-medium">838,80 W</p>
-              <p className="text-2xl font-medium">34034,93€</p>
+              <p className="flex-1 text-xl font-medium">838,80 W</p>
+              <p className="text-xl font-medium">34034,93€</p>
             </div>
           </div>
 
           <div className="relative rounded-xl p-4">
             <div className="absolute inset-0 animate-pulse rounded-xl border-4 border-secondary delay-100"></div>
             <div className="flex">
-              <div className="flex-1 relative z-10 flex gap-x-2">
-                <MaterialSymbol icon="water_drop" size={32}></MaterialSymbol>
-                <p className="text-2xl font-medium">Water</p>
+              <div className="relative z-10 flex flex-1 gap-x-2">
+                <MaterialSymbol icon="water_drop" size={24}></MaterialSymbol>
+                <p className="text-xl font-medium">Water</p>
               </div>
-              <p className="flex-1 text-2xl font-medium">23.2 L</p>
-              <p className="text-2xl font-medium">23.11€</p>
+              <p className="flex-1 text-xl font-medium">23.2 L</p>
+              <p className="text-xl font-medium">23.11€</p>
             </div>
           </div>
         </div>
@@ -44,10 +44,10 @@ export default function Home() {
         <div className="space-y-7 pt-2">
           <button className="flex w-full justify-between rounded-2xl bg-background p-6">
             <div className="space-y-2">
-              <div className="text-3xl font-bold">Main House</div>
-              <div className="center flex gap-1 text-xl font-medium p">
+              <div className="text-2xl font-bold">Main House</div>
+              <div className="center p flex gap-1 text-lg font-medium">
                 <MaterialSymbol icon="location_pin" size={24}></MaterialSymbol>
-                Azores Island
+                <p>Azores Island</p>
               </div>
             </div>
             <div className="place-items-end self-center">
@@ -63,10 +63,10 @@ export default function Home() {
           </button>
           <button className="flex w-full justify-between gap-1 rounded-2xl bg-background p-6">
             <div className="space-y-2">
-              <div className="text-3xl font-bold">Main House</div>
-              <div className="center flex gap-1 text-xl font-medium">
+              <div className="text-2xl font-bold">Main House</div>
+              <div className="center flex gap-1 text-lg font-medium">
                 <MaterialSymbol icon="location_pin" size={24}></MaterialSymbol>
-                Azores Island
+                <p>Azores Island</p>
               </div>
             </div>
             <div className="place-items-end self-center">
@@ -82,10 +82,10 @@ export default function Home() {
           </button>
           <button className="flex w-full justify-between gap-1 rounded-2xl bg-background p-6">
             <div className="space-y-2">
-              <div className="text-3xl font-bold">Main House</div>
-              <div className="center flex gap-1 text-xl font-medium">
+              <div className="text-2xl font-bold">Main House</div>
+              <div className="center flex gap-1 text-lg font-medium">
                 <MaterialSymbol icon="location_pin" size={24}></MaterialSymbol>
-                Azores Island
+                <p>Azores Island</p>
               </div>
             </div>
             <div className="place-items-end self-center">
@@ -99,15 +99,15 @@ export default function Home() {
               </Link>
             </div>
           </button>
-          <div className="flex justify-center items-center gap-4">
-          <Link
-            href="/home"
-            className="flex rounded-lg bg-accent p-4 py-2 text-base font-bold"
-          >
-            <MaterialSymbol icon="add" size={24}></MaterialSymbol>
-            Register
-          </Link>
-        </div>
+          <div className="flex items-center justify-center gap-4">
+            <Link
+              href="/home"
+              className="flex rounded-lg bg-accent p-4 py-2 text-base font-bold"
+            >
+              <MaterialSymbol icon="add" size={24}></MaterialSymbol>
+              Register
+            </Link>
+          </div>
         </div>
       </div>
     </div>
