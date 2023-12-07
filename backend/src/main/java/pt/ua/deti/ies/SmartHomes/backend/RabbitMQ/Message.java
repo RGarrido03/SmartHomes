e78a9d44122139e09ba344c5e618f866fe5286e4
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-12-07 00:50:48 +0000

    Adapt Message class to new data

--- backend/src/main/java/pt/ua/deti/ies/SmartHomes/backend/RabbitMQ/Message.java
+++ backend/src/main/java/pt/ua/deti/ies/SmartHomes/backend/RabbitMQ/Message.java
@@ -6,6 +6,8 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -17,6 +19,15 @@ public class Message {
 
     @NotEmpty
     private Power power;
+
+    @NotEmpty
+    private Water water;
+
+    @NotEmpty
+    private Costs costs;
+
+    @NotEmpty
+    private List<Device> devices;
 }
 
 @NoArgsConstructor
@@ -81,4 +92,64 @@ class House {
 
     @NotEmpty
     private long renewable;
+
+    @NotEmpty
+    private long emissions;
+
+    @NotEmpty
+    private List<Long> renewable_forecast_day;
+
+    @NotEmpty
+    private List<Long> renewable_forecast_hour;
+}
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Data
+class Water {
+    @NotEmpty
+    private long kitchen;
+
+    @NotEmpty
+    private long bath;
+
+    @NotEmpty
+    private long garden;
+
+    @NotEmpty
+    private long other;
+
+    @NotEmpty
+    private long total;
+
+    @NotEmpty
+    private long forecast_today;
+}
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Data
+class Costs {
+    @NotEmpty
+    private long electricity;
+
+    @NotEmpty
+    private long water;
+
+    @NotEmpty
+    private long total;
+}
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Data
+class Device {
+    @NotEmpty
+    private int id;
+
+    @NotEmpty
+    private long power;
 }
