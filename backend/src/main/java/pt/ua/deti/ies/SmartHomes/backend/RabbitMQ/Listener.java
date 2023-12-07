36071ab918f4319f5bc24b02c85c424d947b871d
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-12-07 00:58:49 +0000

    Write new data to InfluxDB

--- backend/src/main/java/pt/ua/deti/ies/SmartHomes/backend/RabbitMQ/Listener.java
+++ backend/src/main/java/pt/ua/deti/ies/SmartHomes/backend/RabbitMQ/Listener.java
@@ -37,8 +37,33 @@ public class Listener {
                            .addField("house_wind", message.getPower().getHouse().getWind())
                            .addField("house_grid_exchange", message.getPower().getHouse().getGrid_exchange())
                            .addField("house_total", message.getPower().getHouse().getTotal())
+
                            .addField("house_self_sufficiency", message.getPower().getHouse().getSelf_sufficiency())
                            .addField("house_renewable", message.getPower().getHouse().getRenewable())
+                           .addField("emissions", message.getPower().getHouse().getEmissions())
+
+                           .addField("renewable_forecast_day_0", message.getPower().getHouse().getRenewable_forecast_day().get(0))
+                           .addField("renewable_forecast_day_1", message.getPower().getHouse().getRenewable_forecast_day().get(1))
+                           .addField("renewable_forecast_day_2", message.getPower().getHouse().getRenewable_forecast_day().get(2))
+                           .addField("renewable_forecast_hour_0", message.getPower().getHouse().getRenewable_forecast_hour().get(0))
+                           .addField("renewable_forecast_hour_1", message.getPower().getHouse().getRenewable_forecast_hour().get(1))
+                           .addField("renewable_forecast_hour_2", message.getPower().getHouse().getRenewable_forecast_hour().get(2))
+
+                           .addField("water_kitchen", message.getWater().getKitchen())
+                           .addField("water_bath", message.getWater().getBath())
+                           .addField("water_garden", message.getWater().getGarden())
+                           .addField("water_other", message.getWater().getOther())
+                           .addField("water_total", message.getWater().getTotal())
+
+                           .addField("costs_electricity", message.getCosts().getElectricity())
+                           .addField("costs_water", message.getCosts().getWater())
+                           .addField("costs_total", message.getCosts().getTotal())
+
+                           .addField("device_0", message.getDevices().get(0).getPower())
+                           .addField("device_1", message.getDevices().get(1).getPower())
+                           .addField("device_2", message.getDevices().get(2).getPower())
+                           .addField("device_3", message.getDevices().get(3).getPower())
+
                            .time(Instant.now(), WritePrecision.NS);
 
         // Write the point to InfluxDB
