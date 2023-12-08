fc0186e251defcd29b8b0756dab1564add27771f
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-12-08 00:28:12 +0000

    Create environment, water & devices mappings

--- backend/src/main/java/pt/ua/deti/ies/SmartHomes/backend/Database/HouseController.java
+++ backend/src/main/java/pt/ua/deti/ies/SmartHomes/backend/Database/HouseController.java
@@ -77,7 +77,7 @@ public class HouseController {
     private QueryApi queryApi;
 
     @GetMapping("{id}/electricity")
-    public ResponseEntity<List<ElectricityData>> getHouseSolar(@PathVariable String id) {
+    public ResponseEntity<List<ElectricityData>> getElectricity(@PathVariable String id) {
         List<ElectricityData> data = new ArrayList<>();
         String parametrizedQuery = String.format(
                 "from(bucket: \"smarthomes\") |> range(start: -1d) |> filter(fn: (r) => r._measurement == \"%s\") |> pivot(rowKey: [\"_time\"], columnKey: [\"_field\"], valueColumn: \"_value\") |> drop(columns: [\"_start\", \"_stop\", \"_measurement\"])", id);
@@ -99,6 +99,109 @@ public class HouseController {
                             (long) fluxRecord.getValueByKey("house_self_sufficiency"),
                             (long) fluxRecord.getValueByKey("house_renewable"));
                     data.add(electricityData);
+                }
+            }
+            if (data.isEmpty()) {
+                code = HttpStatus.NOT_FOUND;
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            code = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+        return new ResponseEntity<>(data, code);
+    }
+
+    @GetMapping("{id}/environment")
+    public ResponseEntity<List<EnvironmentData>> getEnvironment(@PathVariable String id) {
+        List<EnvironmentData> data = new ArrayList<>();
+        String parametrizedQuery = String.format(
+                "from(bucket: \"smarthomes\") |> range(start: -1d) |> filter(fn: (r) => r._measurement == \"%s\") |> pivot(rowKey: [\"_time\"], columnKey: [\"_field\"], valueColumn: \"_value\") |> drop(columns: [\"_start\", \"_stop\", \"_measurement\"])", id);
+
+        List<FluxTable> result = queryApi.query(parametrizedQuery, "smarthomes");
+
+        HttpStatus code = HttpStatus.OK;
+
+        try {
+            for (FluxTable fluxTable : result) {
+                for (FluxRecord fluxRecord : fluxTable.getRecords()) {
+                    EnvironmentData environmentData = new EnvironmentData(
+                            fluxRecord.getTime(),
+                            (long) fluxRecord.getValueByKey("house_self_sufficiency"),
+                            (long) fluxRecord.getValueByKey("house_renewable"),
+                            (long) fluxRecord.getValueByKey("emissions"),
+                            List.of(
+                                (long) fluxRecord.getValueByKey("renewable_forecast_day_0"),
+                                (long) fluxRecord.getValueByKey("renewable_forecast_day_1"),
+                                (long) fluxRecord.getValueByKey("renewable_forecast_day_2")
+                            ),
+                            List.of(
+                                (long) fluxRecord.getValueByKey("renewable_forecast_hour_0"),
+                                (long) fluxRecord.getValueByKey("renewable_forecast_hour_1"),
+                                (long) fluxRecord.getValueByKey("renewable_forecast_hour_2")
+                            ));
+                    data.add(environmentData);
+                }
+            }
+            if (data.isEmpty()) {
+                code = HttpStatus.NOT_FOUND;
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            code = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+        return new ResponseEntity<>(data, code);
+    }
+
+    @GetMapping("{id}/water")
+    public ResponseEntity<List<WaterData>> getWater(@PathVariable String id) {
+        List<WaterData> data = new ArrayList<>();
+        String parametrizedQuery = String.format(
+                "from(bucket: \"smarthomes\") |> range(start: -1d) |> filter(fn: (r) => r._measurement == \"%s\") |> pivot(rowKey: [\"_time\"], columnKey: [\"_field\"], valueColumn: \"_value\") |> drop(columns: [\"_start\", \"_stop\", \"_measurement\"])", id);
+
+        List<FluxTable> result = queryApi.query(parametrizedQuery, "smarthomes");
+
+        HttpStatus code = HttpStatus.OK;
+
+        try {
+            for (FluxTable fluxTable : result) {
+                for (FluxRecord fluxRecord : fluxTable.getRecords()) {
+                    WaterData waterData = new WaterData(
+                            fluxRecord.getTime(),
+                            (long) fluxRecord.getValueByKey("water_kitchen"),
+                            (long) fluxRecord.getValueByKey("water_bath"),
+                            (long) fluxRecord.getValueByKey("water_garden"),
+                            (long) fluxRecord.getValueByKey("water_other"),
+                            (long) fluxRecord.getValueByKey("water_total"),
+                            (long) fluxRecord.getValueByKey("water_forecast_today"));
+                    data.add(waterData);
+                }
+            }
+            if (data.isEmpty()) {
+                code = HttpStatus.NOT_FOUND;
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            code = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+        return new ResponseEntity<>(data, code);
+    }
+
+    @GetMapping("{id}/devices")
+    public ResponseEntity<List<DevicePower>> getDevices(@PathVariable String id) {
+        List<DevicePower> data = new ArrayList<>();
+        String parametrizedQuery = String.format(
+                "from(bucket: \"smarthomes\") |> range(start: -5m) |> filter(fn: (r) => r._measurement == \"%s\") |> last() |> pivot(rowKey: [\"_time\"], columnKey: [\"_field\"], valueColumn: \"_value\") |> drop(columns: [\"_start\", \"_stop\", \"_measurement\"])", id);
+
+        List<FluxTable> result = queryApi.query(parametrizedQuery, "smarthomes");
+
+        HttpStatus code = HttpStatus.OK;
+
+        try {
+            for (FluxTable fluxTable : result) {
+                for (FluxRecord fluxRecord : fluxTable.getRecords()) {
+                    data.add(new DevicePower(0, (long) fluxRecord.getValueByKey("device_0")));
+                    data.add(new DevicePower(1, (long) fluxRecord.getValueByKey("device_1")));
+                    data.add(new DevicePower(2, (long) fluxRecord.getValueByKey("device_2")));
                 }
             }
             if (data.isEmpty()) {
