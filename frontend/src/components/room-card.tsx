cf5b4b72db563e8ff11cb877e7da3b3013af50b0
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-12-10 02:18:08 +0000

    Fix room card design

--- frontend/src/components/room-card.tsx
+++ frontend/src/components/room-card.tsx
@@ -8,6 +8,7 @@ import {
 import { Button } from "@/components/ui/button";
 import { MaterialSymbol } from "react-material-symbols";
 import { Room } from "@/app/home/devices/page";
+import { cn } from "@/lib/utils";
 
 type RoomCardProps = {
   room: Room;
@@ -20,24 +21,24 @@ export function RoomCard({ room }: RoomCardProps) {
         <CardTitle>{room.name}</CardTitle>
         <CardDescription>Using {room.power} W</CardDescription>
       </CardHeader>
-      <CardContent className="flex flex-1 flex-row items-center">
-        <Button variant="outline">
-          <MaterialSymbol icon="mode_fan" size={40} />
-          <p className="text-primary-foreground">AC</p>
-          <p className="text-primary-foreground">420 W</p>
-        </Button>
-        <Button variant="outline">
-          <br />
-          <MaterialSymbol icon="mode_fan" size={40} />
-          <p className="text-primary-foreground">AC</p>
-          <p className="text-primary-foreground">420 W</p>
-        </Button>
-        <Button variant="outline">
-          <br />
-          <MaterialSymbol icon="mode_fan" size={40} />
-          <p className="text-primary-foreground">AC</p>
-          <p className="text-primary-foreground">420 W</p>
-        </Button>
+      <CardContent
+        className={cn(
+          "grid flex-1 grid-cols-3 gap-2",
+          room.devices.length == 1 && "grid-cols-1",
+          room.devices.length == 2 && "grid-cols-2",
+        )}
+      >
+        {room.devices.map((device) => (
+          <Button
+            variant={device.on ? "default" : "secondary"}
+            key={device.id}
+            className="h-fit flex-col gap-1"
+          >
+            <MaterialSymbol icon="mode_fan" size={24} />
+            <p className="font-bold text-foreground">AC</p>
+            <p className="text-secondary-foreground">420 W</p>
+          </Button>
+        ))}
       </CardContent>
     </Card>
   );
