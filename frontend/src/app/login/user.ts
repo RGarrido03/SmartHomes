78dc6db98584137a182b873b3d9e2899031cf024
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-12-10 22:02:21 +0000

    Refactor middleware

---
+++ frontend/src/app/login/user.ts
@@ -0,0 +1,8 @@
+export type User = {
+  token: string;
+  type: string;
+  id: number;
+  expires: number;
+  username: string;
+  email: string;
+};
--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,15 +1,43 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { User } from "./app/login/user";
 
 export const protectedRoutes = ["/home", "/insight"];
 export const authRoutes = ["/login", "/register"];
 
+const isProtected = (path: string): boolean => {
+  let route: string;
+  for (route of protectedRoutes) {
+    if (route === path || path.includes(route)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const isAuth = (path: string): boolean => {
+  let route: string;
+  for (route of authRoutes) {
+    if (route === path || path.includes(route)) {
+      return true;
+    }
+  }
+  return false;
+};
+
 export function middleware(request: NextRequest) {
-  const currentUser = request.cookies.get("currentUser")?.value;
+  const currentUserStr = request.cookies.get("currentUser")?.value;
+  let currentUser: User | null = null;
+
+  if (currentUserStr !== undefined) {
+    currentUser = JSON.parse(currentUserStr);
+  }
+
+  console.log(request.nextUrl.pathname, isProtected(request.nextUrl.pathname));
 
   if (
-    protectedRoutes.includes(request.nextUrl.pathname) &&
-    (!currentUser || Date.now() > JSON.parse(currentUser).expires)
+    isProtected(request.nextUrl.pathname) &&
+    (!currentUserStr || Date.now() > (currentUser?.expires ?? 0))
   ) {
     request.cookies.delete("currentUser");
     const response = NextResponse.redirect(new URL("/login", request.url));
@@ -18,7 +46,7 @@ export function middleware(request: NextRequest) {
     return response;
   }
 
-  if (authRoutes.includes(request.nextUrl.pathname) && currentUser) {
+  if (isAuth(request.nextUrl.pathname) && currentUserStr) {
     return NextResponse.redirect(new URL("/insight", request.url));
   }
 }
