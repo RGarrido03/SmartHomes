bb1ba8b381ac52b52f175c967de23125933552c9
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-12-10 01:36:52 +0000

    Dummy data, aggregate it by room

--- frontend/src/app/home/devices/page.tsx
+++ frontend/src/app/home/devices/page.tsx
@@ -9,11 +9,12 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MaterialSymbol } from "react-material-symbols";
 import { Button } from "@/components/ui/button";
 
 type Device = {
+  id: number;
   name: string;
   type:
     | "TV"
@@ -28,20 +29,75 @@ type Device = {
     | "VACUUM"
     | "DESUMIDIFIER"
     | "OVEN";
+  houseArea: string;
   on: boolean;
+  power: number;
 };
 
 type Room = {
-  //variaveis criadas para demonstrar o pretendido mesmo dando erro
   name: string;
-  watts: number;
-  [key: string]: Device[];
-}[];
+  power: number;
+  devices: Device[];
+};
 
 export default function Devices() {
-  const [data, setData] = useState<Room>([]);
+  const [data, setData] = useState<Device[]>([
+    {
+      id: 1,
+      type: "LIGHT",
+      name: "Light",
+      houseArea: "Living Room",
+      on: false,
+      power: 1114,
+    },
+    {
+      id: 2,
+      type: "AC",
+      name: "Air conditioner",
+      houseArea: "Living Room",
+      on: true,
+      power: 182,
+    },
+    {
+      id: 2,
+      type: "TV",
+      name: "TV",
+      houseArea: "Kitchen",
+      on: true,
+      power: 45,
+    },
+  ]);
 
-  //Objetive create a component (card -content) with this content
+  const [dataByRoom, setDataByRoom] = useState<Room[]>([]);
+
+  // Organize devices by room
+  useEffect(() => {
+    const temp = data.reduce((accumulator: Room[], currentValue: Device) => {
+      let houseArea = accumulator.find(
+        (area) => area.name === currentValue.houseArea,
+      );
+
+      if (!houseArea) {
+        houseArea = {
+          name: currentValue.houseArea,
+          power: 0,
+          devices: [],
+        };
+        accumulator.push(houseArea);
+      }
+
+      houseArea.devices.push(currentValue);
+
+      if (currentValue.on) {
+        houseArea.power += currentValue.power;
+      }
+
+      return accumulator;
+    }, []);
+
+    setDataByRoom(temp);
+  }, [data]);
+
   return (
     <div className="grid grid-flow-row grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
       <TitleCard text="Control your devices to improve your house's energy efficency." />
