e9526078ad257f736fc9a7d2777c686eb4cd6657
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-12-10 04:34:25 +0000

    Payload requests and responses

---
+++ backend/src/main/java/pt/ua/deti/ies/SmartHomes/backend/Security/Payloads/JwtResponse.java
@@ -0,0 +1,21 @@
+package pt.ua.deti.ies.SmartHomes.backend.Security.Payloads;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class JwtResponse {
+    private String token;
+    private String type = "Bearer";
+    private Long id;
+    private String username;
+    private String email;
+
+    public JwtResponse(String accessToken, Long id, String username, String email) {
+        this.token = accessToken;
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+}
