eb7bff632a461faecd3e9af9022b4821fe654530
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-12-10 23:26:50 +0000

    Add logout option

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,8 +1,33 @@
+"use client";
+
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { MaterialSymbol } from "react-material-symbols";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useCookies } from "next-client-cookies";
+import { User } from "@/app/login/user";
+import { useRouter } from "next/navigation";
 
 export default function Navbar() {
+  const cookies = useCookies();
+  const user: User =
+    cookies.get("currentUser") === undefined
+      ? null
+      : JSON.parse(cookies.get("currentUser") ?? "");
+  const router = useRouter();
+
+  const logout = () => {
+    cookies.remove("currentUser");
+    router.push("/login");
+  };
+
   return (
     <div className="flex flex-row items-center justify-between gap-4 p-4">
       <Link href="/insight">
@@ -11,11 +36,24 @@ export default function Navbar() {
         </Button>
       </Link>
       <p className="w-full text-lg font-semibold">Main house</p>
-      <Link href="/profile">
-        <Button variant="default" className="h-fit rounded-full p-1">
-          <MaterialSymbol size={24} icon="account_circle" />
-        </Button>
-      </Link>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="default" className="h-fit rounded-full p-1">
+            <MaterialSymbol size={24} icon="account_circle" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>
+            Hi, {user == null ? "" : user.name.split(" ")[0]}!
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem onClick={logout}>
+            <MaterialSymbol icon="logout" className="mr-2" />
+            Logout
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 }
