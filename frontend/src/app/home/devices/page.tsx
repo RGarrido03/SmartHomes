0ef94b9e68253039cf4b92f0432706a9fd7c1332
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-12-10 00:47:27 +0000

    Use Button instead of Link, indentation

--- frontend/src/app/home/devices/page.tsx
+++ frontend/src/app/home/devices/page.tsx
@@ -1,292 +1,223 @@
-'use client';
+"use client";
 
-import {TitleCard} from "@/components/title-card";
-import {Card, CardContent, CardDescription, CardHeader, CardTitle, } from "@/components/ui/card";
-//impport componente card-content
+import { TitleCard } from "@/components/title-card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
-
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { MaterialSymbol } from "react-material-symbols";
-import Link from "next/link";
-import {buttonVariants} from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 
 type Device = {
-  name: string,
-  type:'TV' | 'SOLARPV' | 'WINDTURBINE' | 'PLUG' | 'GRIDMETER' | 'AC' | 'LIGHT' | 'SMARTASSISTANT' | 'CARCARGER' | 'VACUUM' | 'DESUMIDIFIER'| 'OVEN',
-    on: boolean
+  name: string;
+  type:
+    | "TV"
+    | "SOLARPV"
+    | "WINDTURBINE"
+    | "PLUG"
+    | "GRIDMETER"
+    | "AC"
+    | "LIGHT"
+    | "SMARTASSISTANT"
+    | "CARCARGER"
+    | "VACUUM"
+    | "DESUMIDIFIER"
+    | "OVEN";
+  on: boolean;
 };
 
 type Room = {
-    //variaveis criadas para demonstrar o pretendido mesmo dando erro
-    name: string,
-    watts: number,
-    [key:string]: Device[]
-
+  //variaveis criadas para demonstrar o pretendido mesmo dando erro
+  name: string;
+  watts: number;
+  [key: string]: Device[];
 }[];
 
-export default function Devices(){
-    const [data, setData] = useState<Room>([]);
+export default function Devices() {
+  const [data, setData] = useState<Room>([]);
 
-    //Objetive create a component (card -content) with this content
-    return <div className="grid grid-flow-row grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-        <TitleCard text="Control your devices to improve your house's energy efficency."/>
+  //Objetive create a component (card -content) with this content
+  return (
+    <div className="grid grid-flow-row grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+      <TitleCard text="Control your devices to improve your house's energy efficency." />
 
-        <Card className="overflow-hidden">
-            <CardHeader icon="scene">
-                <CardTitle>
-                    Room Name
-                </CardTitle>
-                <CardDescription>
-                    Using {data.length !== 0 ? data[data.length - 1].watts : "0"} W
-                </CardDescription>
-            </CardHeader>
-            <CardContent className="flex flex-1 flex-row items-center">
-                    <Link className={buttonVariants({variant: "outline"})} href="#">
-                        <MaterialSymbol icon="mode_fan" size={40}/>
-                        <p className="text-primary-foreground">
-                            AC
-                        </p>
-                        <p className="text-primary-foreground">
-                            420 W
-                        </p>
-                    </Link>
-                    <Link className={buttonVariants({variant: "outline"})} href="#">
-                        <br/>
-                        <MaterialSymbol icon="mode_fan" size={40}/>
-                        <p className="text-primary-foreground">
-                            AC
-                        </p>
-                        <p className="text-primary-foreground">
-                            420 W
-                        </p>
-                    </Link>
-                    <Link className={buttonVariants({variant: "outline"})} href="#">
-                        <br/>
-                        <MaterialSymbol icon="mode_fan" size={40}/>
-                        <p className="text-primary-foreground">
-                            AC
-                        </p>
-                        <p className="text-primary-foreground">
-                            420 W
-                        </p>
-                    </Link>
-            </CardContent>
-        </Card>
+      <Card className="overflow-hidden">
+        <CardHeader icon="scene">
+          <CardTitle>Room Name</CardTitle>
+          <CardDescription>
+            Using {data.length !== 0 ? data[data.length - 1].watts : "0"} W
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="flex flex-1 flex-row items-center">
+          <Button variant="outline">
+            <MaterialSymbol icon="mode_fan" size={40} />
+            <p className="text-primary-foreground">AC</p>
+            <p className="text-primary-foreground">420 W</p>
+          </Button>
+          <Button variant="outline">
+            <br />
+            <MaterialSymbol icon="mode_fan" size={40} />
+            <p className="text-primary-foreground">AC</p>
+            <p className="text-primary-foreground">420 W</p>
+          </Button>
+          <Button variant="outline">
+            <br />
+            <MaterialSymbol icon="mode_fan" size={40} />
+            <p className="text-primary-foreground">AC</p>
+            <p className="text-primary-foreground">420 W</p>
+          </Button>
+        </CardContent>
+      </Card>
 
-        <Card className="overflow-hidden">
-            <CardHeader icon="scene">
-                <CardTitle>
-                Room Name
-                </CardTitle>
-                <CardDescription>
-                    Using {data.length !== 0 ? data[data.length - 1].watts : "0"} W
-                </CardDescription>
-            </CardHeader>
-            <CardContent className="flex flex-1 flex-row items-center">
-                <Link className={buttonVariants({variant: "outline"})} href="#">
-                    <br/>
-                    <MaterialSymbol icon="mode_fan" size={40}/>
-                    <p className="text-primary-foreground">
-                        AC
-                    </p>
-                    <p className="text-primary-foreground">
-                        420 W
-                    </p>
-                </Link>
-                <Link className={buttonVariants({variant: "outline"})} href="#">
-                    <br/>
-                    <MaterialSymbol icon="mode_fan" size={40}/>
-                    <p className="text-primary-foreground">
-                        AC
-                    </p>
-                    <p className="text-primary-foreground">
-                        420 W
-                    </p>
-                </Link>
-                <Link className={buttonVariants({variant: "outline"})} href="#">
-                    <br/>
-                    <MaterialSymbol icon="mode_fan" size={40}/>
-                    <p className="text-primary-foreground">
-                        AC
-                    </p>
-                    <p className="text-primary-foreground">
-                        420 W
-                    </p>
-                </Link>
-            </CardContent>
-        </Card>
+      <Card className="overflow-hidden">
+        <CardHeader icon="scene">
+          <CardTitle>Room Name</CardTitle>
+          <CardDescription>
+            Using {data.length !== 0 ? data[data.length - 1].watts : "0"} W
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="flex flex-1 flex-row items-center">
+          <Button variant="outline">
+            <br />
+            <MaterialSymbol icon="mode_fan" size={40} />
+            <p className="text-primary-foreground">AC</p>
+            <p className="text-primary-foreground">420 W</p>
+          </Button>
+          <Button variant="outline">
+            <br />
+            <MaterialSymbol icon="mode_fan" size={40} />
+            <p className="text-primary-foreground">AC</p>
+            <p className="text-primary-foreground">420 W</p>
+          </Button>
+          <Button variant="outline">
+            <br />
+            <MaterialSymbol icon="mode_fan" size={40} />
+            <p className="text-primary-foreground">AC</p>
+            <p className="text-primary-foreground">420 W</p>
+          </Button>
+        </CardContent>
+      </Card>
 
-        <Card className="overflow-hidden">
-            <CardHeader icon="scene">
-                <CardTitle>
-                    Room Name
-                </CardTitle>
-                <CardDescription>
-                    Using {data.length !== 0 ? data[data.length - 1].watts : "0"} W
-                </CardDescription>
-            </CardHeader>
-            <CardContent className="flex flex-1 flex-row items-center">
-                <Link className={buttonVariants({variant: "outline"})} href="#">
-                    <br/>
-                    <MaterialSymbol icon="mode_fan" size={40}/>
-                    <p className="text-primary-foreground">
-                        AC
-                    </p>
-                    <p className="text-primary-foreground">
-                        420 W
-                    </p>
-                </Link>
-                <Link className={buttonVariants({variant: "outline"})} href="#">
-                    <br/>
-                    <MaterialSymbol icon="mode_fan" size={40}/>
-                    <p className="text-primary-foreground">
-                        AC
-                    </p>
-                    <p className="text-primary-foreground">
-                        420 W
-                    </p>
-                </Link>
-                <Link className={buttonVariants({variant: "outline"})} href="#">
-                    <br/>
-                    <MaterialSymbol icon="mode_fan" size={40}/>
-                    <p className="text-primary-foreground">
-                        AC
-                    </p>
-                    <p className="text-primary-foreground">
-                        420 W
-                    </p>
-                </Link>
-            </CardContent>
-        </Card>
+      <Card className="overflow-hidden">
+        <CardHeader icon="scene">
+          <CardTitle>Room Name</CardTitle>
+          <CardDescription>
+            Using {data.length !== 0 ? data[data.length - 1].watts : "0"} W
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="flex flex-1 flex-row items-center">
+          <Button variant="outline">
+            <br />
+            <MaterialSymbol icon="mode_fan" size={40} />
+            <p className="text-primary-foreground">AC</p>
+            <p className="text-primary-foreground">420 W</p>
+          </Button>
+          <Button variant="outline">
+            <br />
+            <MaterialSymbol icon="mode_fan" size={40} />
+            <p className="text-primary-foreground">AC</p>
+            <p className="text-primary-foreground">420 W</p>
+          </Button>
+          <Button variant="outline">
+            <br />
+            <MaterialSymbol icon="mode_fan" size={40} />
+            <p className="text-primary-foreground">AC</p>
+            <p className="text-primary-foreground">420 W</p>
+          </Button>
+        </CardContent>
+      </Card>
 
-        <Card className="overflow-hidden">
-            <CardHeader icon="scene">
-                <CardTitle>
-                    Room Name
-                </CardTitle>
-                <CardDescription>
-                    Using {data.length !== 0 ? data[data.length - 1].watts : "0"} W
-                </CardDescription>
-            </CardHeader>
-            <CardContent className="flex flex-1 flex-row items-center">
-                <Link className={buttonVariants({variant: "outline"})} href="#">
-                    <br/>
-                    <MaterialSymbol icon="mode_fan" size={40}/>
-                    <p className="text-primary-foreground">
-                        AC
-                    </p>
-                    <p className="text-primary-foreground">
-                        420 W
-                    </p>
-                </Link>
-                <Link className={buttonVariants({variant: "outline"})} href="#">
-                    <br/>
-                    <MaterialSymbol icon="mode_fan" size={40}/>
-                    <p className="text-primary-foreground">
-                        AC
-                    </p>
-                    <p className="text-primary-foreground">
-                        420 W
-                    </p>
-                </Link>
-                <Link className={buttonVariants({variant: "outline"})} href="#">
-                    <br/>
-                    <MaterialSymbol icon="mode_fan" size={40}/>
-                    <p className="text-primary-foreground">
-                        AC
-                    </p>
-                    <p className="text-primary-foreground">
-                        420 W
-                    </p>
-                </Link>
-            </CardContent>
-        </Card>
+      <Card className="overflow-hidden">
+        <CardHeader icon="scene">
+          <CardTitle>Room Name</CardTitle>
+          <CardDescription>
+            Using {data.length !== 0 ? data[data.length - 1].watts : "0"} W
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="flex flex-1 flex-row items-center">
+          <Button variant="outline">
+            <br />
+            <MaterialSymbol icon="mode_fan" size={40} />
+            <p className="text-primary-foreground">AC</p>
+            <p className="text-primary-foreground">420 W</p>
+          </Button>
+          <Button variant="outline">
+            <br />
+            <MaterialSymbol icon="mode_fan" size={40} />
+            <p className="text-primary-foreground">AC</p>
+            <p className="text-primary-foreground">420 W</p>
+          </Button>
+          <Button variant="outline">
+            <br />
+            <MaterialSymbol icon="mode_fan" size={40} />
+            <p className="text-primary-foreground">AC</p>
+            <p className="text-primary-foreground">420 W</p>
+          </Button>
+        </CardContent>
+      </Card>
 
-        <Card className="overflow-hidden">
-            <CardHeader icon="scene">
-                <CardTitle>
-                    Room Name
-                </CardTitle>
-                <CardDescription>
-                    Using {data.length !== 0 ? data[data.length - 1].watts : "0"} W
-                </CardDescription>
-            </CardHeader>
-            <CardContent className="flex flex-1 flex-row items-center">
-                <Link className={buttonVariants({variant: "outline"})} href="#">
-                    <br/>
-                    <MaterialSymbol icon="mode_fan" size={40}/>
-                    <p className="text-primary-foreground">
-                        AC
-                    </p>
-                    <p className="text-primary-foreground">
-                        420 W
-                    </p>
-                </Link>
-                <Link className={buttonVariants({variant: "outline"})} href="#">
-                    <br/>
-                    <MaterialSymbol icon="mode_fan" size={40}/>
-                    <p className="text-primary-foreground">
-                        AC
-                    </p>
-                    <p className="text-primary-foreground">
-                        420 W
-                    </p>
-                </Link>
-                <Link className={buttonVariants({variant: "outline"})} href="#">
-                    <br/>
-                    <MaterialSymbol icon="mode_fan" size={40}/>
-                    <p className="text-primary-foreground">
-                        AC
-                    </p>
-                    <p className="text-primary-foreground">
-                        420 W
-                    </p>
-                </Link>
-            </CardContent>
-        </Card>
+      <Card className="overflow-hidden">
+        <CardHeader icon="scene">
+          <CardTitle>Room Name</CardTitle>
+          <CardDescription>
+            Using {data.length !== 0 ? data[data.length - 1].watts : "0"} W
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="flex flex-1 flex-row items-center">
+          <Button variant="outline">
+            <br />
+            <MaterialSymbol icon="mode_fan" size={40} />
+            <p className="text-primary-foreground">AC</p>
+            <p className="text-primary-foreground">420 W</p>
+          </Button>
+          <Button variant="outline">
+            <br />
+            <MaterialSymbol icon="mode_fan" size={40} />
+            <p className="text-primary-foreground">AC</p>
+            <p className="text-primary-foreground">420 W</p>
+          </Button>
+          <Button variant="outline">
+            <br />
+            <MaterialSymbol icon="mode_fan" size={40} />
+            <p className="text-primary-foreground">AC</p>
+            <p className="text-primary-foreground">420 W</p>
+          </Button>
+        </CardContent>
+      </Card>
 
-        <Card className="overflow-hidden">
-            <CardHeader icon="scene">
-                <CardTitle>
-                    Room Name
-                </CardTitle>
-                <CardDescription>
-                    Using {data.length !== 0 ? data[data.length - 1].watts : "0"} W
-                </CardDescription>
-            </CardHeader>
-            <CardContent className="flex flex-1 flex-row items-center">
-                <Link className={buttonVariants({variant: "outline"})} href="#">
-                    <br/>
-                    <MaterialSymbol icon="mode_fan" size={40}/>
-                    <p className="text-primary-foreground">
-                        AC
-                    </p>
-                    <p className="text-primary-foreground">
-                        420 W
-                    </p>
-                </Link>
-                <Link className={buttonVariants({variant: "outline"})} href="#">
-                    <br/>
-                    <MaterialSymbol icon="mode_fan" size={40}/>
-                    <p className="text-primary-foreground">
-                        AC
-                    </p>
-                    <p className="text-primary-foreground">
-                        420 W
-                    </p>
-                </Link>
-                <Link className={buttonVariants({variant: "outline"})} href="#">
-                    <br/>
-                    <MaterialSymbol icon="mode_fan" size={40}/>
-                    <p className="text-primary-foreground">
-                        AC
-                    </p>
-                    <p className="text-primary-foreground">
-                        420 W
-                    </p>
-                </Link>
-            </CardContent>
-        </Card>
-
-    </div>;
+      <Card className="overflow-hidden">
+        <CardHeader icon="scene">
+          <CardTitle>Room Name</CardTitle>
+          <CardDescription>
+            Using {data.length !== 0 ? data[data.length - 1].watts : "0"} W
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="flex flex-1 flex-row items-center">
+          <Button variant="outline">
+            <br />
+            <MaterialSymbol icon="mode_fan" size={40} />
+            <p className="text-primary-foreground">AC</p>
+            <p className="text-primary-foreground">420 W</p>
+          </Button>
+          <Button variant="outline">
+            <br />
+            <MaterialSymbol icon="mode_fan" size={40} />
+            <p className="text-primary-foreground">AC</p>
+            <p className="text-primary-foreground">420 W</p>
+          </Button>
+          <Button variant="outline">
+            <br />
+            <MaterialSymbol icon="mode_fan" size={40} />
+            <p className="text-primary-foreground">AC</p>
+            <p className="text-primary-foreground">420 W</p>
+          </Button>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
