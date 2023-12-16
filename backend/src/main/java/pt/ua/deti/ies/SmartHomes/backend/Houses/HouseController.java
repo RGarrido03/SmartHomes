ca07e13a6cec34bb1348c0eb2546bba685b29eb9
Brupez <63374228+Brupez@users.noreply.github.com>
2023-12-16 10:20:46 +0000

    Operations summarys and API responses updated (HouseController)

--- backend/src/main/java/pt/ua/deti/ies/SmartHomes/backend/Houses/HouseController.java
+++ backend/src/main/java/pt/ua/deti/ies/SmartHomes/backend/Houses/HouseController.java
@@ -1,5 +1,10 @@
 package pt.ua.deti.ies.SmartHomes.backend.Houses;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +17,7 @@ import pt.ua.deti.ies.SmartHomes.backend.Devices.DeviceService;
 import pt.ua.deti.ies.SmartHomes.backend.RabbitMQ.Sender;
 
 @RestController
+@RestControllerAdvice
 @AllArgsConstructor
 @RequestMapping("api/houses")
 public class HouseController {
@@ -20,12 +26,26 @@ public class HouseController {
     private DeviceService deviceService;
     private Sender sender;
 
+
+    @Operation(summary = "Get house by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "House found",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = HouseController.class)) }),
+
+            @ApiResponse(responseCode = "400", description = "Invalid house id supplied",
+                    content = @Content),
+
+            @ApiResponse(responseCode = "404", description = "House not found",
+                    content = @Content) })
     @GetMapping("{id}")
     public ResponseEntity<House> getHouse(@PathVariable("id") long id) {
         House house = houseService.getHouse(id);
         return new ResponseEntity<>(house, house != null ? HttpStatus.OK : HttpStatus.NOT_FOUND);
     }
 
+    @Operation(summary = "Creation of house")
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public ResponseEntity<House> createHouse(@RequestBody House house) {
         House savedDevice = houseService.createHouse(house);
@@ -33,6 +53,17 @@ public class HouseController {
         return new ResponseEntity<>(savedDevice, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Edit house info by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "House edited",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = HouseController.class)) }),
+
+            @ApiResponse(responseCode = "400", description = "Invalid house id supplied",
+                    content = @Content),
+
+            @ApiResponse(responseCode = "404", description = "House not found",
+                    content = @Content) })
     @PutMapping("{id}")
     public ResponseEntity<House> updateHouse(@PathVariable("id") long houseId, @RequestBody House house) {
         house.setHouseId(houseId);
@@ -41,6 +72,8 @@ public class HouseController {
         return new ResponseEntity<>(updatedHouse, updatedHouse != null ? HttpStatus.OK : HttpStatus.NOT_FOUND);
     }
 
+    @Operation(summary = "Deletion of house by id")
+    @ResponseStatus(HttpStatus.OK)
     @DeleteMapping("{id}")
     public ResponseEntity<String> deleteHouse(@PathVariable("id") long houseId) {
         House house = houseService.getHouse(houseId);
