b8a97ea1700c44e40d2d0b4cca860afe38292214
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-12-18 02:23:45 +0000

    Delete user support

--- frontend/src/app/home/settings/page.tsx
+++ frontend/src/app/home/settings/page.tsx
@@ -6,7 +6,18 @@ import { Button } from "@/components/ui/button";
 import { useCookies } from "next-client-cookies";
 import { User } from "@/app/login/user";
 import { useRouter } from "next/navigation";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import { useToast } from "@/components/ui/use-toast";
 
 type Option = {
   title: string;
@@ -16,6 +27,7 @@ type Option = {
 };
 
 export default function Home() {
+  const { toast } = useToast();
   const router = useRouter();
   const cookies = useCookies();
   const user: User =
@@ -28,26 +40,28 @@ export default function Home() {
     router.push("/login");
   }, [cookies, router]);
 
-  function deleteUser(userId: number) {
-    fetch(`localhost/api/clients/${userId}`, {
-      method: "DELETE",
-      headers: {
-        Authorization: "Bearer " + user.token,
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("Success:", data);
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  }
+  const [openDeleteModal, setOpenDeleteModal] = useState<boolean>(false);
 
-  async function handleDelete() {
-    deleteUser(user.id);
-    logout();
-  }
+  const deleteUser = async () => {
+    const result = await fetch(
+      `http://${process.env.NEXT_PUBLIC_HOST_URL}/api/clients/${user.id}`,
+      {
+        method: "DELETE",
+        headers: {
+          Authorization: "Bearer " + user.token,
+        },
+      },
+    );
+    if (result.status === 200) {
+      logout();
+    } else {
+      toast({
+        title: "Error deleting user.",
+        description: "Check the console for more logs.",
+        variant: "destructive",
+      });
+    }
+  };
 
   const options: Option[] = [
     {
@@ -98,10 +112,30 @@ export default function Home() {
           </p>
           <p>This app is proudly made in the EU. So are the rules.</p>
         </div>
-        <Button className="p-2" variant="destructive" onClick={handleDelete}>
+        <Button
+          className="p-2"
+          variant="destructive"
+          onClick={() => setOpenDeleteModal(true)}
+        >
           <MaterialSymbol icon="delete" size={24} />
         </Button>
       </div>
+
+      <AlertDialog open={openDeleteModal} onOpenChange={setOpenDeleteModal}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete all
+              your data.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction onClick={deleteUser}>Delete</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </div>
   );
 }
