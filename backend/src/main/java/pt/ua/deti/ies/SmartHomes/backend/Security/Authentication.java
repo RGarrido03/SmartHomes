42b85493f4df5f4d37704ac8e89d08bb976ce1d4
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-12-18 16:01:46 +0000

    Set API info

--- backend/src/main/java/pt/ua/deti/ies/SmartHomes/backend/Security/Authentication.java
+++ backend/src/main/java/pt/ua/deti/ies/SmartHomes/backend/Security/Authentication.java
@@ -2,9 +2,7 @@ package pt.ua.deti.ies.SmartHomes.backend.Security;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -88,11 +86,8 @@ public class Authentication {
                                                      addList("Bearer Authentication"))
                             .components(new Components().addSecuritySchemes
                                                                 ("Bearer Authentication", createAPIKeyScheme()))
-                            .info(new Info().title("My REST API")
-                                            .description("Some custom description of API.")
-                                            .version("1.0").contact(new Contact().name("Sallo Szrajbman")
-                                                                                 .email( "www.baeldung.com").url("salloszraj@gmail.com"))
-                                            .license(new License().name("License of API")
-                                                                  .url("API license URL")));
+                            .info(new Info().title("SmartHomes API")
+                                            .description("Every API endpoint used in the app.")
+                                            .version("1.0"));
     }
 }
